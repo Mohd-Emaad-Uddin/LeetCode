466ee086b9bb3ff32fce3c308f5d4b26bd2987ab
Mohd-Emaad-Uddin <emaadmohd81@gmail.com>
2026-09-11 19:20:35 +0530

    Time: 925 ms (5.01%), Space: 47.8 MB (90.95%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -10,49 +10,51 @@ class Pair implements Comparable<Pair>{
     public int compareTo(Pair p) {
         if(this.freq == p.freq)
             return Character.compare(this.ch, p.ch);
-        return Integer.compare(p.freq, this.freq);
+        return Integer.compare(this.freq, p.freq);
     } 
 }
 
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] freq = new int[26];
+        int[] hash = new int[26];
         int[] free = new int[26];
 
         for(char ch: tasks) {
-            freq[ch-'A']++;
-            free[ch-'A'] = 1;
+            hash[ch - 'A']++;
+            free[ch - 'A'] = 1;
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0; i<26; i++) {
-            if(freq[i] > 0)
-                pq.add(new Pair((char)(i + 'A'), freq[i]));
+            if(hash[i] > 0)
+                pq.add(new Pair((char)(i + 'A'), hash[i]));
         }
 
         int seats = 1;
         while(!pq.isEmpty()) {
             List<Pair> temp = new ArrayList<>();
+
             while(!pq.isEmpty()) {
                 Pair p = pq.remove();
-                char c = p.ch;
+
+                char ch = p.ch;
                 int fr = p.freq;
 
-                if(free[c - 'A'] <= seats) {
-                    if(fr > 1)
-                        pq.add(new Pair(c, fr-1));
+                if(free[ch - 'A'] <= seats) {
+                    if(fr > 1) 
+                        pq.add(new Pair(ch, fr - 1));
                     
-                    free[c - 'A'] = seats + n + 1;
+                    free[ch - 'A'] = seats + n + 1;
                     break;
                 }
 
                 temp.add(p);
             }
-            
+
             for(Pair p: temp) {
                 pq.add(p);
             }
-           
+
             seats++;
         }
 
