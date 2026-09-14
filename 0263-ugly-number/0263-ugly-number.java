e994bc72dd3ede62174073e42bffab5347d56f13
Mohd-Emaad-Uddin <emaadmohd81@gmail.com>
2026-09-14 16:23:38 +0530

    Time: 1 ms (99.11%), Space: 42.1 MB (96.6%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0)
+            return false;
+        int[] arr = {2, 3, 5};
+        for(int num: arr) {
+            while(n % num == 0)
+                n = n / num;
+        }
+
+        return n == 1;
+    }
+}
