ab14c91779ec74a7351d05bc36face891caf4187
Mohd-Emaad-Uddin <emaadmohd81@gmail.com>
2026-09-15 18:58:43 +0530

    Time: 2 ms (97.48%), Space: 72.4 MB (49.32%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for(int num: bills) {
+            if(num == 5)
+                five++;
+            else if(num == 10) {
+                if(five > 0) {
+                    five -= 1;
+                    ten++;
+                }
+                else
+                    return false;
+            }
+
+            else {
+                if(ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if(five > 2)
+                    five -= 3;
+                else
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
